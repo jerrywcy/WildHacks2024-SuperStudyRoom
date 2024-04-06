eace70c53df6ce68b36c51be6139d9fb8dfe88b1
jerrywcy <jerrymonkeynj@163.com>
2024-04-06 17:58:20 -0500

    feat: change to mui; start on main page; write example for api

---
+++ app/api/study_room/search/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server";
+
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  const dateParam = searchParams.get("date");
+  let date: Date | undefined;
+  if (!dateParam) date = undefined;
+  else date = new Date(dateParam);
+
+  const startParam = searchParams.get("start");
+  let start: Date | undefined;
+  if (!startParam) start = undefined;
+  else start = new Date(startParam);
+
+  const endParam = searchParams.get("end");
+  let end: Date | undefined;
+  if (!endParam) end = undefined;
+  else end = new Date(endParam);
+
+  const timeParam = searchParams.get("time");
+  let time: number | undefined;
+  if (!timeParam) time = undefined;
+  else time = Number(timeParam);
+
+  const capacityParam = searchParams.get("time");
+  let capacity: number | undefined;
+  if (!capacityParam) capacity = undefined;
+  else capacity = Number(capacityParam);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +20,11 @@ export default function RootLayout({
     return (
         <html lang="en" className="dark">
             <body className={inter.className}>
-                <Providers>
-                    {children}
-                </Providers>
+                <AppRouterCacheProvider>
+                    <ThemeProvider theme={theme}>
+                        {children}
+                    </ThemeProvider>
+                </AppRouterCacheProvider>
             </body>
         </html>
     );
